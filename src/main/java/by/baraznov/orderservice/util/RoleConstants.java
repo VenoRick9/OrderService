3150236d01c715b726e03d9927f4a732a81f01ae
VenoRick9 <danila.baraznou@innowise.com>
2025-10-26 19:08:49 +0300

    rewrite role logic

---
+++ src/main/java/by/baraznov/orderservice/util/RoleConstants.java
@@ -0,0 +1,7 @@
+package by.baraznov.orderservice.util;
+
+public final class RoleConstants {
+    public static final String ROLE_USER = "ROLE_USER";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+    private RoleConstants() {}
+}
